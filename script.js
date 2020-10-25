b757fb7fa391f457af289ec9fabf49ae09d748d8
a11ce <a11ce@protonmail.com>
2020-10-25 02:46:08 -0400

    Replace and improve js, split wordlists into own file, allow user selection of wordlist

---
+++ script.js
@@ -0,0 +1,87 @@
+window.onload = function() {
+    resetState()
+    listChoices = document.getElementById("listChoices")
+    for (var key in listsDict) {
+        var newOption = document.createElement("option");
+        newOption.text = key;
+        listChoices.add(newOption);
+    }
+    document.getElementById("pass").addEventListener("click", pass, false);
+    document.getElementById("success").addEventListener("click", success, false);
+    document.getElementById("startbutton").addEventListener("click", newGame, false);
+}
+
+var timer;
+var time;
+
+var sCount = 0;
+var pCount = 0;
+
+function newGame() {
+    if (!timer) {
+        curList = listsDict[document.getElementById("listChoices").value]
+        time = document.getElementById("startingTime").value;
+        document.getElementById("listChoices").disabled = true;
+        document.getElementById("startingTime").disabled = true;
+        newWord();
+
+
+        timer = setInterval(function() {
+            time--;
+            if (time == 0) {
+                endGame();
+            }
+            document.getElementById("timer").innerHTML = time;
+        }, 1000);
+    } else {
+        resetState();
+        newGame();
+    }
+}
+
+
+function endGame() {
+    document.getElementById("curItem").innerHTML = "Game Over!";
+    clearInterval(timer);
+    timer = null;
+    document.getElementById("listChoices").disabled = false;
+    document.getElementById("startingTime").disabled = false;
+}
+
+function resetState() {
+    pCount = 0;
+    sCount = 0;
+
+    clearInterval(timer);
+    time = document.getElementById("startingTime").value;
+
+    document.getElementById("successcount").innerHTML = sCount;
+    document.getElementById("passcount").innerHTML = sCount;
+
+    document.getElementById("timer").innerHTML = time;
+    timer = null;
+}
+
+function success() {
+    if (timer) {
+        sCount++;
+        document.getElementById("successcount").innerHTML = sCount;
+        newWord()
+    }
+
+}
+
+function pass() {
+    if (timer) {
+        pCount++;
+        document.getElementById("passcount").innerHTML = pCount;
+        newWord()
+    }
+
+}
+
+function newWord() {
+    console.log(listsDict);
+    curItem = curList[Math.floor(Math.random() * curList.length)];
+    document.getElementById("curItem").innerHTML = curItem;
+}
